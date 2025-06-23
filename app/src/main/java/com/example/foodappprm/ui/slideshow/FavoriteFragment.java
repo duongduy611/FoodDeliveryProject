5ff018e95ba9d1e3147ca0b7d2c959c50a84f72b
DuongVietDuy <duydv.forwork@gmail.com>
2025-06-24 00:31:37 +0700

    xong welcome page

---
+++ app/src/main/java/com/example/foodappprm/ui/slideshow/FavoriteFragment.java
@@ -0,0 +1,22 @@
+package com.example.foodappprm.ui.slideshow;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.foodappprm.R;
+
+public class FavoriteFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        return root;
+    }
+}
